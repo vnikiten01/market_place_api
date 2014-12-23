ac1ad7ed949fd7c2be312bd7d657e283cd58f6b8
Viktor Nikitenko <vnikiten01@gmail.com>
2014-12-23 16:25:41 -0500

    Deletes extra end line 7

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     scope module: :v1 do
       end
     end
-  end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
