bdfa6b610087a672188d2b0f38208ea565eeee0a
Viktor Nikitenko <vnikiten01@gmail.com>
2014-12-23 17:01:45 -0500

    Fixes route.rb syntax issues

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ require 'api_contraints'
 
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api'}, path: '/' do
-    scope module: :v1 do
-      constraints: ApiConstraints.new(version: 1, default: true) do
-      end
+    scope module: :v1,
+                constraints: ApiConstraints.new(version: 1, default: true) do
     end
   end
 
